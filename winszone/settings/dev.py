e334fa762b040aea9f6bb1c1ad8e5ff3366f20a7
Hamza Zahir <hzahir47@gmail.com>
2024-06-14 18:21:30 +0500

    first commit

---
+++ winszone/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-2tqc8(##i1ubwlyi5-(cap33n!kq5s#4ff3_4!_np-4g1u0bry"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
