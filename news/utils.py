ae087244ba874630d811e791b15eec9df06796de
Celeron24 <74257152+Celeron24@users.noreply.github.com>
2024-06-24 17:36:42 +0500

    Delete news directory

--- news/utils.py
+++
@@ -1,9 +0,0 @@
-import requests
-from django.core.files.base import ContentFile
-
-
-def save_image_from_url(url, title, file_name):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return ContentFile(response.content, name=file_name)
-    return None
